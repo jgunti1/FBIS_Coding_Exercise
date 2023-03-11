321c9be4de854a2bf778b8b41e7cad534ad74744
Jordan Gunti <jgunti1@gmail.com>
2023-03-11 10:14:59 -0600

    Initial Commit

---
+++ Mailing Task/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+public interface ILetterService 
+{
+    /// <summary>
+    /// Combine two letter files into one file.
+    /// </summary>
+    /// <param name="inputFile1">File path for the first letter.</param>
+    /// <param name="inputFile2">File path for the second letter.</param>
+    /// <param name="resultFile">File path for the combined letter.</param>
+    void CombineTwoLetters(string inputFile1,string inputFile2, string resultFile);
+}
+public class LetterService : ILetterService
+{
+    public void CombineTwoLetters(string inputFile1 ,string inputFile2, string resultFile) 
+    {
+        // My implementation here.
+        // Merge the text content in the two input files
+        // and write the merged content to the output file.
+
+        // Assuming current project is in CombinedLetters directory
+        const string folder = "Input";
+        const string admission = "Admission";
+        const string scholarships = "Scholarships";
+        
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FBIS_Coding_Exercise
+This is the Technical Screen Exercise for Jordan Gunti.
+Estimated Hours Spent on This task: 24 hours, This includes any learning that needs to take place and errors that should come up. I am also basing this off an expected work week of 8 hours per day or 3 days for this task. 
+
+
